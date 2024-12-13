95af26060b80601f00626100a48f37edc2001bd9
Romain Giovanetti <ganoninc@gmail.com>
2024-12-13 13:55:33 +0100

    add sliding window approach to 219.ts

--- 219.ts
+++ 219.ts
@@ -1,17 +1,36 @@
 // Hashmap version
-function containsNearbyDuplicate(nums: number[], k: number): boolean {
-  let tracker: Map<number, number[]> = new Map();
+// function containsNearbyDuplicate(nums: number[], k: number): boolean {
+//   let tracker: Map<number, number[]> = new Map();
 
-  for (let i = 0; i < nums.length; i++) {
-    if (tracker.has(nums[i])) {
-      for (let passedIndexes of tracker.get(nums[i])!) {
-        if (Math.abs(passedIndexes - i) <= k) return true;
-      }
-    } else {
-      tracker.set(nums[i], []);
+//   for (let i = 0; i < nums.length; i++) {
+//     if (tracker.has(nums[i])) {
+//       for (let passedIndexes of tracker.get(nums[i])!) {
+//         if (Math.abs(passedIndexes - i) <= k) return true;
+//       }
+//     } else {
+//       tracker.set(nums[i], []);
+//     }
+
+//     tracker.get(nums[i])?.push(i);
+//   }
+
+//   return false;
+// }
+
+// sliding window
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  let window: Set<number> = new Set();
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    if (Math.abs(left - right) > k) {
+      window.delete(nums[left]);
+      left++;
     }
 
-    tracker.get(nums[i])?.push(i);
+    if (window.has(nums[right])) return true;
+
+    window.add(nums[right]);
   }
 
   return false;
