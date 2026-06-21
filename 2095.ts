849db32814e87cdb616e6f94ff57a490ec4998d6
Romain Giovanetti <ganoninc@gmail.com>
2026-06-21 18:55:00 +0200

    add 2095.ts

---
+++ 2095.ts
@@ -0,0 +1,26 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteMiddle(head: ListNode): ListNode | null {
+  if (!head.next) return null;
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+  let prev: ListNode | null = null;
+
+  while (fast !== null && fast.next) {
+    prev = slow;
+    slow = slow!.next;
+    fast = fast.next.next;
+  }
+
+  prev!.next = slow!.next;
+
+  return head;
+}
