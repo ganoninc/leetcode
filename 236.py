616cc30b8e928bb5750baeea1d7e170ee24a5d8a
Romain Giovanetti <ganoninc@gmail.com>
2026-06-25 18:14:38 +0200

    update 236.py

--- 236.py
+++ 236.py
@@ -9,21 +9,25 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def dfs(self, node: Optional[TreeNode], p, q,)
+    lowest_common_ancestor = TreeNode(0)
+
+    def dfs(self, node: Optional[TreeNode], p: TreeNode, q: TreeNode) -> int:
+        if node is None:
+            return 0
+        
+        found_count = 0
+        if node.val == p.val or node.val == q.val:
+            found_count += 1
+
+        found_count += self.dfs(node.left, p, q) + self.dfs(node.right, p, q)
+
+        if found_count == 2:
+            self.lowest_common_ancestor = node
+            found_count = 0
+
+        return found_count
 
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-
-        if root is None:
-            return
-        
-        if root == p:
-            return root
-        
-        if root == q:
-            return root
-        
-        q_in_branch = None
-        p_in_branch = None
-        if root.left:
-            q_in_branch = self.lowestCommonAncestor(root.left, p, q)
-            p_in_branch = self.lowestCommonAncestor(root.left, p, q)
+        self.lowest_common_ancestor = TreeNode(0)
+        self.dfs(root, p, q)
+        return self.lowest_common_ancestor
